4701489251678041d5e9b72613b8c60f6365b93f
eunho8713 <dmsgh3303@gmail.com>
2022-07-14 18:33:47 +0900

    first upload

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          "Hello World"
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='h1-size'>
+        Hello.
+        <img className='img' alt='plane' src='image/airplane.png' width={300} height={380}></img>
+      </h1>
+      
+        
+      <h2>
+        My name is LEE EUN HO
+      </h2>
+      <h>
+      Hello, I'm Lee Eunho, a developer and ambitious person.
+      <br></br>I like to work alone or with others in a free and original atmosphere.
+      <br></br>But acting responsibly is the most important thing. 
+      <br></br>It is rewarding to create an app/web that is convenient and easy for users to use. 
+      <br></br>I like to think of new ideas.
+      <br></br>I communicate with people, have meetings, and enjoy social activities.
+      <br></br>I enjoy all sports and sports, and I like soccer and weight training the most among them.
+      </h>
+      
+    </>
+
   );
 }
 
